7ebb43c7b035453230f7de60311cb80b3b4b470c
sunil chauhan <35426939+sunilchauhan4all@users.noreply.github.com>
2018-05-20 08:50:22 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,43 +1,46 @@
-
 window.addEventListener("load",bindEvents);
 var buttonArray;
 function bindEvents(){
-  buttonArray= document.getElementsByTagName("button");
-  for(let i=0; i<buttonArray.length;i++){
-    buttonArray[i].addEventListener("click",printXorZero);
-  }
-}
-
-const isNotBlank=(button)=>button.innerHTML?true:false;
-
-const isSameValue=(one,two,three)=>(one.innerHtml==two.innerHtml && one.innerHtml==three.innerHtml);
-
-var isGameDone=false;
-
-function isSameRow(one,two,three){
-  if(isNotBlank(one)&& isNotBlank(two) && isNotBlank(three)){
-    if (isSameValue(one,two,three)){
-      
-       return true;
+    buttonArray = document.getElementsByTagName("button");
+    for(let  i =0; i<buttonArray.length; i++){
+        buttonArray[i].addEventListener("click",printXorZero);
     }
-    return false;
-  }
 }
+const isNotBlank = (button)=>button.innerHTML?true:false;
+
+const isSameValue=(one,two,three)=>(one.innerHTML == two.innerHTML && one.innerHTML == three.innerHTML);
+
+var isGameDone = false;
+
+function isSameRow(one , two ,three){
+if(isNotBlank(one) && isNotBlank(two) && isNotBlank(three)){
+if(isSameValue(one,two,three)){
+    return true;
+}
+return false;
+}
+}
+
 
 function gameOver(){
-  if(isSameRow(buttonArray[0],buttonArray[1],buttonArray[2])){
-       isGameDone=true;
-       alert("game over");
-     }
+    if(isSameRow(buttonArray[0],buttonArray[1],buttonArray[2])){
+        isGameDone= true;
+        alert("Game Over");
+    }
+    if(isSameRow(buttonArray[0],buttonArray[3],buttonArray[6])){
+        isGameDone= true;
+        alert("Game Over");
+    }
 }
 
-
-var isXorZero=false;
+var isXorZero = false;
 function printXorZero(){
-  var buttonValue=this.innerHtml;
-  if(!buttonValue && !isGameDone){
-       this.innerHtml=isXorZero?"X":"0";
-       isXorZero= !isXorZero;
-       gameOver();
-  }
+    //console.log("Testing...",this);
+    var buttonValue = this.innerHTML;
+    if(!buttonValue && !isGameDone){
+            this.innerHTML = isXorZero?"X":"0";
+            isXorZero = !isXorZero;
+            gameOver();
+    }
 }
+
