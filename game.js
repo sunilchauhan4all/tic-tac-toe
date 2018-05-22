8583e1fb59e11c42161afe04820e008c3bc78b0b
sunil chauhan <35426939+sunilchauhan4all@users.noreply.github.com>
2018-05-22 13:22:44 +0530

    Update game.js

--- game.js
+++ game.js
@@ -55,7 +55,7 @@ function gameOver(){
 }
 
 var isXorZero = false;
-Var person="sunil";
+var person="sunil";
 function printXorZero(){
     //console.log("Testing...",this);
     var buttonValue = this.innerHTML;
